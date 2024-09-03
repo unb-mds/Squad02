04486c2a08c1aff87dae38c309451a2d66708b38
sidts <enricozoratto36@gmail.com>
2024-09-02 21:15:35 -0300

    Mudei o regex, agora está buscando os valores corretamente

--- backend/functions/regex.py
+++ backend/functions/regex.py
@@ -1,12 +1,12 @@
 def regexMunicipio(municipio):
     regex_dict = {
-        'arraial do cabo': r'Secretaria Municipal de Educação, Cultura, Ciência e Tecnologia.*R\$\s*((?:\d+\.\d+|\d+\.\d*|\.\d+|\d+,\d+))\b',
-        'belford roxo': r'SECRETARIA MUNICIPAL DE CULTURA.*R\$\s*((?:\d+\.\d+|\d+\.\d*|\.\d+|\d+,\d+))\b',
-        'niteroi': r'SECRETARIA MUNICIPAL DAS CULTURAS.*R\$\s*((?:\d+\.\d+|\d+\.\d*|\.\d+|\d+,\d+))\b',
-        'nova iguaçu': r'Secretaria Municipal de Cultura.*?(\d{1,3}(?:\.\d{3})*,\d{2})\b',
-        'rio de janeiro': r'SECRETARIA MUNICIPAL DE CULTURA.*R\$\s*((?:\d+\.\d+|\d+\.\d*|\.\d+|\d+,\d+))\b',
-        'são josé de mereti': r'Secretaria municipal de educação, cultura e turismo.*R\$\s*((?:\d+\.\d+|\d+\.\d*|\.\d+|\d+,\d+))\b',
+        'arraial do cabo': r'(?:Secretaria Municipal de Educação, Cultura, Ciência e Tecnologia|cultura|tecnologia|educação|show|rede)[^R$]*R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))',
+        'belford roxo': r'(?:SECRETARIA MUNICIPAL DE CULTURA)[^R$]*R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))',
+        'niteroi': r'(?:SECRETARIA MUNICIPAL DAS CULTURAS)[^R$]*R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))',
+        'nova iguaçu': r'(?:Secretaria Municipal de Cultura)[^R$]*R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))',
+        'rio de janeiro': r'(?:SECRETARIA MUNICIPAL DE CULTURA)[^R$]*R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))',
+        'são josé de mereti': r'(?:Secretaria Municipal de Educação, Cultura e Turismo|Secretaria Municipal de Cultura, Lazer, Direitos Humanos e Igualdade Racial|Secretaria Municipal de Cultura e Turismo|Secretaria Municipal de Cultura, Turismo, Direitos Humanos e Igualdade Racial|cultura)[^R$]*R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))',
     }
-    regex_padrao = r'Secretaria Municipal.*?R\$\s*((?:\d+\.\d+|\d+\.\d*|\.\d+|\d+,\d+))\b'
+    regex_padrao = r'Secretaria Municipal.*?R\$\s*((?:\d{1,3}(?:\.\d{3})*,\d{2}|\d+,\d{2}))\b'
 
     return regex_dict.get(municipio, regex_padrao)
